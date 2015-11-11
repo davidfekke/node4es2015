e76a998edc7be6f32a6f7eaf9fe04571230c220e
David Fekke <david@fekke.com>
2015-11-11 14:27:24 -0500

    Adding ES5 example

--- example8-spread.js
+++ example8-spread.js
@@ -8,3 +8,7 @@ let inner = [3, 4];
 let merged = [0, 1, 2, ...inner, 5]; //Spread operator will fail if run in Node 4.X
 
 console.log(merged);
+
+// In ES5 you would have to do this by using the concat method.
+// var inner = [3, 4];
+// var merged = [0, 1, 2].concat(inner, [5]);
